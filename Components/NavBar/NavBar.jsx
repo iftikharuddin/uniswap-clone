151711785992a3d0b798f9127069f374f28d2e8a
Iftikhar uddin <iftikhar@connextar.com>
2023-11-01 23:42:40 +0500

    Setting up NavBar and Modals

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
-import React, {useState, useEffect, useContext} from 'react';
+'use client';
+import React, { useState, useEffect, useContext } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-// import internal
-import Style from './NavBar.module.css';
-import images from '../../assets';
-import {Model, TokenList} from '../index';
+//IMPORT INTERNAL
+import Style from "./NavBar.module.css";
+import images from "../../assets";
+import { Model, TokenList } from "../index";
 
-function NavBar(props) {
+//CONTEXT
+// import { SwapTokenContext } from "../../Context/SwapContext";
+
+const NavBar = () => {
+    // const { ether, account, networkConnect, connectWallet, tokenData } =
+        // useContext(SwapTokenContext);
     const menuItems = [
         {
             name: "Swap",
@@ -21,27 +27,68 @@ function NavBar(props) {
             name: "Pools",
             link: "/",
         },
-        {
-            name: "Tokens",
-            link: "/",
-        },
     ];
-
-    // usestate
+    //USESTATE
     const [openModel, setOpenModel] = useState(false);
     const [openTokenBox, setOpenTokenBox] = useState(false);
+
     return (
         <div className={Style.NavBar}>
             <div className={Style.NavBar_box}>
                 <div className={Style.NavBar_box_left}>
-                    Left
+                    {/* //LOGO IMAGE  */}
+                    <div className={Style.NavBar_box_left_img}>
+                        <Image src={images.uniswap} alt="logo" width={50} height={50} />
+                    </div>
+                    {/* MENU ITEMS */}
+
+                    <div className={Style.NavBar_box_left_menu}>
+                        {menuItems.map((el, i) => (
+                            <Link key={i + 1} href={{ pathname: `${el.name}` }}>
+                                <p className={Style.NavBar_box_left_menu_item}>{el.name}</p>
+                            </Link>
+                        ))}
+                    </div>
                 </div>
+                {/* //Middle SECTION */}
+                <div className={Style.NavBar_box_middle}>
+                    <div className={Style.NavBar_box_middle_search}>
+                        <div className={Style.NavBar_box_middle_search_img}>
+                            <Image src={images.search} alt="search" width={20} height={20} />
+                        </div>
+                        {/* //INPUT SECTION */}
+                        <input type="text" placeholder="Search Tokens" />
+                    </div>
+                </div>
+                {/* //RIGHT SECTION */}
                 <div className={Style.NavBar_box_right}>
-                    Right
+                    <div className={Style.NavBar_box_right_box}>
+                        <div className={Style.NavBar_box_right_box_img}>
+                            <Image src={images.ether} alt="NetWork" height={30} width={30} />
+                        </div>
+                        {/*<p>{networkConnect}</p>*/}
+                        <p>Network Name</p>
+                    </div>
+                    {/*{account ? (*/}
+                    {/*    <button onClick={() => setOpenTokenBox(true)}>*/}
+                    {/*        {account.slice(0, 20)}..*/}
+                    {/*    </button>*/}
+                    {/*) : (*/}
+                        <button onClick={() => setOpenModel(true)}>Connect</button>
+                    {/*)}*/}
+
+                    {openModel && (
+                        <Model setOpenModel={setOpenModel} connectWallet="Address" />
+                    )}
                 </div>
             </div>
+
+             {/*//TOTENLIST COMPONENT*/}
+            {openTokenBox && (
+                <TokenList tokenDate={tokenData} setOpenTokenBox={setOpenTokenBox} />
+            )}
         </div>
     );
-}
+};
 
 export default NavBar;
